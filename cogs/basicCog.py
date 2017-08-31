b8be978070e23f3dd6c613ea2fbb0601e96eff39
JonasUJ <jonasuj@hotmail.dk>
2017-08-31 08:07:51 +0200

    Add my_id to secrets.json

--- cogs/basicCog.py
+++ cogs/basicCog.py
@@ -1,21 +1,23 @@
 import discord
 from discord.ext import commands
+from utils import Utils
 
 
 class basicCog:
 
     def __init__(self, bot):
         self.bot = bot
+        self.utils = Utils(self.bot)
 
     
     @commands.command()
-    async def ping(self, ctx, *, content):
+    async def ping(self, ctx):
         await ctx.send('Pong')
 
     
     @commands.command()
     async def exit(self, ctx):
-        if ctx.author.id == 1234567890:
+        if ctx.author.id == self.utils.config['my_id']:
             await self.bot.close()
         else:
             await ctx.send('Permission denied')
