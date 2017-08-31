5b76e5d86373f8b4b65c5853952f7e3e9ee6f5e2
JonasUJ <jonasuj@hotmail.dk>
2017-08-31 21:41:41 +0200

    Add cog code.py

---
+++ cogs/code.py
@@ -0,0 +1,70 @@
+import re
+import traceback
+import os
+import discord
+from subprocess import Popen, PIPE
+from tempfile import TemporaryFile
+from discord.ext import commands
+from utils import Utils
+
+
+class Code:
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.utils = Utils(self.bot)
+
+        # RegularExpression for detecting python code markdown
+        self.code_expr = re.compile(r'```py\n([^```]+)')
+
+    
+    @commands.command()
+    async def process(self, ctx, *, to_process):
+        '''Process the code passed in to_process and edit ctx.message with output'''
+
+        try:
+            # Get code to run
+            to_exec = self.code_expr.search(to_process).group(1).encode('utf-8')
+        except AttributeError:
+            # If the code markdown was improperly formatted
+            return
+
+        # Send message confirming that the snippet is being processed
+        final_msg = await ctx.send('Processing...\n{}'.format(to_process))
+
+        # Create tempFile and write code to it
+        tempFile = TemporaryFile(delete=False)
+        tempFile.write(to_exec)
+        tempFile.close()
+
+        # Open tempFile with Python
+        process = Popen(['python', tempFile.name], shell=True, stdout=PIPE, stderr=PIPE)
+
+        # Get output
+        out = process.stdout.read().decode('utf-8')
+        err = process.stderr.read().decode('utf-8')
+
+        # Remove tempFile
+        os.remove(tempFile.name)
+
+        # Format output
+        formatted_output = '```py\n{}{}```'.format(out, err)
+        if formatted_output == '```py\n```': formatted_output = '```No output```'
+        new_msg = '{}\nOutput:{}'.format(to_process, formatted_output)
+
+        try:
+            # Edit message
+            await final_msg.edit(content=new_msg)
+        except discord.errors.HTTPException:
+            await final_msg.edit(content='Failed\n{}'.format(to_process))
+        
+
+   
+    @commands.command()
+    async def eval(self, ctx, *, expr):
+        '''Evaluate an expression'''
+        pass
+
+
+def setup(bot):
+    bot.add_cog(Code(bot))
