9bf81bd078617f4269748fc92003782013698153
JonasUJ <jonasuj@hotmail.dk>
2017-09-17 14:41:34 +0200

    Add sound (in progess) and ffmpeg.exe

--- cogs/sound.py
+++ cogs/sound.py
@@ -1,6 +1,7 @@
 """Cog for a discord bot, uses a discord.VoiceClient to play sounds"""
 
-from tempfile import TemporaryFile
+import asyncio
+from tempfile import NamedTemporaryFile
 
 import discord
 from discord.ext import commands
@@ -17,22 +18,31 @@ class Sound:
     def __init__(self, bot):
         self.bot = bot
         self.utils = Utils(self.bot)
+        self.voice_clients = list()
+
+
+    def disconnect(self, voice_index):
+        """Disconnect from VoiceChannel"""
+
+        coro = self.voice_clients.pop(voice_index).disconnect()
+        fut = asyncio.run_coroutine_threadsafe(coro, self.bot.loop)
+        fut.result()
 
 
     @commands.command()
-    async def play(self, ctx, *args):
+    async def play(self, ctx, *, args=None):
         """Play a `mp3` file that was sent over discord as an attachment"""
 
         if not ctx.author.voice:
             return await ctx.send("You're not connected to a voice channel.")
 
-        with TemporaryFile() as fp:
-            attfile = await ctx.message.attachments[0].save(fp)
-            to_play = discord.PCMAudio(attfile)
+        with NamedTemporaryFile(delete=False) as fp:
+            await ctx.message.attachments[0].save(fp)
+        to_play = discord.FFmpegPCMAudio(fp.name)
 
-            voice = await ctx.author.voice.channel.connect()
-            voice.play(to_play)
-            
+        voice = await ctx.author.voice.channel.connect()
+        self.voice_clients.append(voice)
+        voice.play(to_play, after=lambda: self.disconnect(self.voice_clients.index(voice)))
 
 
 def setup(bot):
